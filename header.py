082d2ae5eba6ed4f7cf495162363c99e02bd1e22
kajtek <kajtek0123@gmail.com>
2024-01-14 14:53:23 +0100

    page navigation

--- header.py
+++ header.py
@@ -1,16 +1,19 @@
 import sys
+from os import system, name
 import binascii
 
-# by default every page contains 16 lines made of 32 characters (16 bytes)
-options = {
-        "line_length":32,
-        "page_size":16
+vars = {
+        # by default every page contains 16 lines made of 32 characters
+        "line_length":32, # (16 bytes)
+        "page_size":16,
+        "column_width":4,
+        "current_page":0
         }
 
 # raw_contents = variable of the bytes type containing original file contents
 def processContents(raw_contents):
-    line_length = options["line_length"]
-    page_size = options["page_size"]
+    line_length = vars["line_length"]
+    page_size = vars["page_size"]
 
     # bytes type -> bytes type, hexadecimal notation (hexlify()) 
     # -> utf-8 string (decode())
@@ -46,15 +49,48 @@ def handleArgs():
         exit()
 
 def printPage(contents, page_index=0):
-    line_length = options["line_length"]
-    page_size = options["page_size"]
+    line_length = vars["line_length"]
+    page_size = vars["page_size"]
 
     # first byte of the line index (hexdump-like)
-    byte_index = page_index * page_size * line_length
+    byte_index = page_index * page_size * int(line_length/2)
 
     for line in contents[page_index]:
         # C string style of formating for padding the first byte of the 
         # line index
-        print('%07x'%byte_index + " " + line)
+        line_to_print = '%07x'%byte_index + " "
+        # printing contents as column_width wide columns
+        line_to_print += " ".join(line[i:i+vars["column_width"]] for i in range(0, len(line), vars["column_width"]))
+
+        print(line_to_print)
+
         byte_index += int(line_length / 2)
+
+def clear():
+    if name == "nt":
+        _ = system("cls")
+    else:
+        _ = system('clear')
+
+def userInput(contents):
+    ch = input()
+    match ch:
+        # show next page
+        case 'n':
+            if vars["current_page"] < len(contents) - 1:
+                vars["current_page"] += 1
+                clear()
+                printPage(contents, vars["current_page"])
+        # show previous page
+        case 'p':
+            if vars["current_page"] > 0:
+                vars["current_page"] -= 1
+                clear()
+                printPage(contents, vars["current_page"])
+        # quit
+        case 'q':
+            clear()
+            exit()
+        case _:
+            print("Unknown command")
 
--- nibbler.py
+++ nibbler.py
@@ -1,13 +1,15 @@
 from header import *
 
 if __name__ == '__main__':
-    is_running = True
-
     handleArgs()
 
     # open file and read its contents
     file_name = sys.argv[1]
     contents = openFile(file_name)
 
-    printPage(contents, 0)
+    clear()
+    printPage(contents)
+
+    while True:
+        userInput(contents)
 
