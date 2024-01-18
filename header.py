aaca80787140cbe01fe55bfd05842e0bc8bacced
kajtek <kajtek0123@gmail.com>
2024-01-18 22:56:58 +0100

    added editing single byte values

--- header.py
+++ header.py
@@ -34,6 +34,7 @@ def processContents(raw_contents):
     
     return contents
 
+# open and read from a file
 def openReadFile(file_name):
     try:
         with open(file_name, 'rb') as f:
@@ -43,13 +44,14 @@ def openReadFile(file_name):
         print(f"Error - couldn't open file {file_name}")
         print(error)
 
+# open and write to a file
 def openWriteFile(file_name, contents):
     contents_string = ""
     for page in contents:
         for line in page:
             contents_string += line
 
-    # return binary data
+    # return to binary data
     contents_raw = binascii.unhexlify(contents_string)
 
     try:
@@ -60,21 +62,22 @@ def openWriteFile(file_name, contents):
         print(f"Error - couldn't open file {file_name}")
         print(error)
 
+# handling command line arguments
 def handleArgs():
     if len(sys.argv) < 2:
         print("Invalid amount of arguments - use nibbler.py -h for help")
         exit()
 
+# print page of the contents
 def printPage(contents, page_index=0):
     line_length = vars["line_length"]
     page_size = vars["page_size"]
 
-    # first byte of the line index (hexdump-like)
+    # address of the first byte on the line (hexdump-like)
     byte_index = page_index * page_size * int(line_length/2)
 
     for line in contents[page_index]:
-        # C string style of formating for padding the first byte of the 
-        # line index
+        # C string style of formating for padding the address with 0s 
         line_to_print = '%07x'%byte_index + " "
         # printing contents as column_width wide columns
         line_to_print += " ".join(line[i:i+vars["column_width"]] for i in range(0, len(line), vars["column_width"]))
@@ -103,14 +106,35 @@ def pageIndexFromAddr(addr):
 
     return int(page_index)
 
-def replaceByte(page_index):
-    try:
-        val = int(input())
-        hex_val = hex(val)
-        hex_string = str(hex_val)
-    except:
-        print(1)
+def replaceSingleByte(contents, page_index, addr):
+    page_size = vars["page_size"]
+    line_length = int(vars["line_length"]/2)
 
+    try:
+        addr = int(addr, 16)
+
+        line_index = int((addr % (page_size * line_length)) / line_length)
+
+        byte_index = addr % line_length
+        byte_index = (byte_index * 2)
+
+        if byte_index in range(len(contents[page_index][line_index])):
+            val = input("New value (hexadecimal): ")
+
+            if int(val, 16) in range(256):
+                # replacing values
+                line = contents[page_index][line_index]
+                line = line[:byte_index] + val.lower() + line[byte_index + 2:]
+                contents[page_index][line_index] = line
+            else:
+                print("Out of range value")
+                return
+        else:
+            print("Out of range value")
+            return
+    except Exception as error:
+        print("Couldn't perform replace operation")
+    
 def userInput(contents):
     try:
         ch = input()
@@ -143,20 +167,51 @@ def userInput(contents):
                     print("Out of range address")
             except Exception as error:
                 print("usage: goto [address]")
+        # replace single byte
         case ['r', *_]:
             args = ch.split()
             try:
-                page_index = pageIndexFromAddr(args[1])
-                line_index = args[1] 
+                addr = args[1] 
+                page_index = pageIndexFromAddr(addr)
                 if page_index in range(len(contents)):
-                    replaceByte(page_index)
+                    vars["current_value"] = page_index
+
+                    clear()
+                    printPage(contents, vars["current_value"])
+
+                    replaceSingleByte(contents, page_index, addr)
+
+                    clear()
+                    printPage(contents, vars["current_value"])
                 else:
                     print("Out of range address")
             except Exception as error:
                 print("usage: r [address]")
-
-        case ['w']:
-            openWriteFile(vars["file_name"], contents)
+                print(error)
+        # change the number of bytes displayed in one column
+        case ['column', 'width', *_]:
+            args = ch.split()
+            if len(args) == 3:
+                try:
+                    val = int(args[2])
+                    if int(val) > 0:
+                        vars["column_width"] = val*2
+                        clear()
+                        printPage(contents, vars["current_page"])
+                    else:
+                        raise Exception
+                except Exception as error:
+                    print("invalid column width value")
+            else:
+                print("usage: column width [value]")
+        case ['w', *_]:
+            args = ch.split()
+            if len(args) == 1:
+                openWriteFile(vars["file_name"], contents)
+            elif len(args) == 2:
+                openWriteFile(args[1], contents)
+            else:
+                print("usage: w [filename(optional)]")
         case ['wq']:
             openWriteFile(vars["file_name"], contents)
             clear()
@@ -166,7 +221,8 @@ def userInput(contents):
             print("n - next page")
             print("p - previous page")
             print("goto [address] - go to specified position")
-            print("r [addr] - edit bytes on specified addr")
+            print("r [addr] - replace byte on specified address")
+            print("column width [value] - change the number of bytes displayed in one column")
             print("w - write")
             print("wq - write and quit")
             print("q - quit")
